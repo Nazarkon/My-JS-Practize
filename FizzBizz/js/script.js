ff155a2400bc2896efc3185b01e804c83292e248
Nazarkon <nazarmontsibovych@gmail.com>
2019-07-14 02:46:15 +0300

    FizzBazz Game

---
+++ FizzBizz/js/script.js
@@ -0,0 +1,20 @@
+let maxNumber = 100;
+let minNumber = 1;
+let arr = [];
+function createNumbers() {
+    while (minNumber <= maxNumber) {
+
+        arr.push(minNumber++)
+    }
+    return arr;
+}
+console.log(createNumbers());
+
+function checkNumbers() {
+    for (let i = 1; i < arr.length; i++) {
+       if(Number.isInteger(arr[i] / 3) && Number.isInteger(arr[i] / 5) ){
+           console.log('FizzBuzz' + arr[i])
+       }
+    }
+}
+console.log(checkNumbers())
